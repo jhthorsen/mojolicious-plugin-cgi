b0bdde6deefec8eb92809469ffca2b055c36752e
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-05-14 10:29:24 +0200

    Released version 0.08
           * Flushing STDERR as well
           * Fix REMOTE_HOST test on windows
             http://www.cpantesters.org/cpan/report/99cac79a-725d-1014-82ca-1dcb5cf4dae8
             REMOTE_HOST=599DSOLIMANO01.cowen.corp != localhost

--- lib/Mojolicious/Plugin/CGI.pm
+++ lib/Mojolicious/Plugin/CGI.pm
@@ -6,7 +6,7 @@ Mojolicious::Plugin::CGI - Run CGI script from Mojolicious
 
 =head1 VERSION
 
-0.07
+0.08
 
 =head1 DESCRIPTION
 
@@ -45,7 +45,7 @@ use constant CHUNK_SIZE => 131072;
 use constant CHECK_CHILD_INTERVAL => $ENV{CHECK_CHILD_INTERVAL} || 0.01;
 use constant DEBUG => $ENV{MOJO_PLUGIN_CGI_DEBUG} || 0;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 our %ORIGINAL_ENV = %ENV;
 
 =head1 METHODS
