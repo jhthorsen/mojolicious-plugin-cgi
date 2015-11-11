035d5e3047f5ab97e11cab08b77534c9fcf781df
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-11-11 22:16:37 +0100

    Released version 0.24
     - Add "run" option for running CGI code from a callback instead of script #17
     - Add HTTP_IF_NONE_MATCH to environement #17
     - Add support for reading the "Status" header
     - Improved NPH script support #17

--- lib/Mojolicious/Plugin/CGI.pm
+++ lib/Mojolicious/Plugin/CGI.pm
@@ -6,7 +6,7 @@ Mojolicious::Plugin::CGI - Run CGI script from Mojolicious
 
 =head1 VERSION
 
-0.23
+0.24
 
 =head1 DESCRIPTION
 
@@ -89,7 +89,7 @@ use constant DEBUG                => $ENV{MOJO_PLUGIN_CGI_DEBUG} || 0;
 use constant READ                 => 0;
 use constant WRITE                => 1;
 
-our $VERSION      = '0.23';
+our $VERSION      = '0.24';
 our %ORIGINAL_ENV = %ENV;
 
 =head1 ATTRIBUTES
